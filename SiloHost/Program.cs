0fd5dc622a85c3298a132a50710523ed50108769
Thiago Loureiro <thiagoguaru@outlook.com>
2018-04-07 12:58:34 +0200

    Initial Commit, SiloHost and Grain

---
+++ SiloHost/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Runtime;
+using Orleans.Statistics;
+using Grains;
+
+// ReSharper disable MethodSupportsCancellation
+
+namespace SiloHost
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var siloPort = 11111;
+            int gatewayPort = 30000;
+            var siloAddress = IPAddress.Loopback;
+
+            var silo =
+                new SiloHostBuilder()
+                    .UseDashboard(options =>
+                    {
+                        options.HostSelf = true;
+                        options.HideTrace = false;
+                    })
+                    .UseDevelopmentClustering(options => options.PrimarySiloEndpoint = new IPEndPoint(siloAddress, siloPort))
+                    .UseInMemoryReminderService()
+                    .ConfigureEndpoints(siloAddress, siloPort, gatewayPort)
+                    .Configure<ClusterOptions>(options => options.ClusterId = "helloworldcluster")
+                    .ConfigureApplicationParts(appParts => appParts.AddApplicationPart(typeof(TestCalls).Assembly))
+                    .ConfigureLogging(builder =>
+                    {
+                        builder.AddConsole();
+                    })
+                    .Build();
+
+            silo.StartAsync().Wait();
+
+            var client =
+                new ClientBuilder()
+                    .UseStaticClustering(options => options.Gateways.Add((new IPEndPoint(siloAddress, gatewayPort)).ToGatewayUri()))
+                    .Configure<ClusterOptions>(options => options.ClusterId = "helloworldcluster")
+                    .ConfigureApplicationParts(appParts => appParts.AddApplicationPart(typeof(TestCalls).Assembly))
+                    .ConfigureLogging(builder =>
+                    {
+                        builder.AddConsole();
+                    })
+                    .Build();
+
+            client.Connect().Wait();
+
+            var cts = new CancellationTokenSource();
+
+            TestCalls.Make(client, cts);
+
+            Console.WriteLine("Press key to exit...");
+            Console.ReadLine();
+
+            cts.Cancel();
+
+            silo.StopAsync().Wait();
+        }
+    }
+}
